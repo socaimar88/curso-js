da624569a80acf2cd98a6ab1dd7907fec5e38e0d
freddo <fred_solis@soy.sena.co>
2024-03-15 00:20:42 -0500

    Utilizamos prompt y if-else

--- js/practica1.js
+++ js/practica1.js
@@ -5,3 +5,18 @@ const SALARIO=1300000;
 let Saludo= "Hola "+ nombre + " "+ apellido;
 console.log(Saludo);
 console.log("Su sarario es: ",SALARIO);
+
+// ejercicio de utilizacion de prompt y if
+const salarioMinimo=1300000;
+let nombreEmpleado;
+let salarioEmpleado;
+
+nombreEmpleado=prompt("Ingrese el nombre del empleado");
+salarioEmpleado=prompt("Ingrese el salario del empleado");
+
+if (salarioEmpleado<= salarioMinimo*2){
+    alert("El Empleado "+nombreEmpleado+" Si aplica auxilio de trasporte");
+}
+else{
+    alert("El Empleado "+nombreEmpleado+" No aplica auxilio de trasporte");
+}
